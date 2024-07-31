53dca1243fa340fb1ea720d847512cb7137f3278
EDWDEV03 <edwdev03@gmail.com>
2024-07-31 09:55:10 -0500

    Add files via upload

---
+++ hola_mundo .py
@@ -0,0 +1,15 @@
+print("Hola Mundo")
+print(int(1+1))
+print((5*3)+(9/3))
+# Tener en cuenta la prioridad de las operaciones, tener en cuenta potenciación, multiplicación y división; suma y resta y de izquierda a derecha
+#Tipos de variable en python
+
+numnericas= 23
+flotantes= 3.19
+cadenas= "Soy una cadena de texto String"
+booleanos= True 
+
+print(numnericas)
+print(flotantes)
+print(cadenas)
+print(booleanos)
